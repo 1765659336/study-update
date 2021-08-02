81aff926d554d313dcbdcda83bb66bab512a73e3
liujie <1765659336@qq.com>
2021-08-02 23:55:59 +0800

    Vue脚手架的学习

---
+++ 03Vue脚手架/04Ref标签属性的使用/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<img src="./assets/logo.png" alt="logo" ref="img">
+		<School ref="school"></School>
+		<School id="school"></School>
+		<button @click="btn">获取ref和id</button>
+	</div>
+</template>
+
+<script>
+	//引入组件
+	import School from './components/School'
+
+	export default {
+		name:'App',
+		components:{
+			School,
+		},
+		methods: {
+			btn(){
+				console.log(this.$refs);
+				console.log(document.getElementById('school'));
+			}
+		},
+	}
+</script>
