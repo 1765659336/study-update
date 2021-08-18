6a740bb8b540bf2a9f1972b955c73ab86bf1c80f
liujie <1765659336@qq.com>
2021-08-18 20:17:09 +0800

    818

--- hzero-fornt/src/config/routers.ts
+++ hzero-fornt/src/config/routers.ts
@@ -1,17 +1,26 @@
-import { RoutersConfig } from "hzero-boot/lib/typings/IRouterConfig";
+import { RoutersConfig } from 'hzero-boot/lib/typings/IRouterConfig';
+
 const routerConfig: RoutersConfig = [
   {
-    path: "/demo-table",
-    component: () => import("../pages/demo-table"),
+    path: '/source_zip/3/f3121c1c631c443c9ef665f78061ce21/rich-text-form2',
+    component: () =>
+      import('../pages/source_zip/3/f3121c1c631c443c9ef665f78061ce21/rich-text-form2'),
     models: [],
-    title: "demoTable",
+    title: 'source_zip/3/f3121c1c631c443c9ef665f78061ce21/rich-text-form',
+    authorized: true,
+  },
+  {
+    path: '/demo-table',
+    component: () => import('../pages/demo-table'),
+    models: [],
+    title: 'demoTable',
     authorized: true,
   }, // Insert New Router
   {
-    path: "/hzero-boot/test1",
-    component: () => import("../pages/test1/Test1Page"),
+    path: '/hzero-boot/test1',
+    component: () => import('../pages/test1/Test1Page'),
     authorized: true,
-    title: "区块测试页",
+    title: '区块测试页',
   },
 ];
 export default routerConfig;
